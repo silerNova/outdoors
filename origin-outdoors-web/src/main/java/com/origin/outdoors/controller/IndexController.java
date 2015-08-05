5d998adfd745f36af79dea3e930a977aa460231d
cengwei29 <zengwei3@jd.com>
2015-08-05 21:52:54 +0800

    提交代码

--- origin-outdoors-web/src/main/java/com/origin/outdoors/controller/IndexController.java
+++ origin-outdoors-web/src/main/java/com/origin/outdoors/controller/IndexController.java
@@ -1,5 +1,17 @@
 package com.origin.outdoors.controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
 public class IndexController {
 
+	@RequestMapping("")
+	public String index(HttpServletRequest request, HttpServletResponse response) {
+		return "home";
+	}
 }
